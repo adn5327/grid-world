f98401c23696fde4e96be0045f05dd72689b03f8
Abhishek Deep Nigam <adn5327@gmail.com>
2015-12-05 15:48:37 -0600

    adding the policy to look at value iteration

--- policy.py
+++ policy.py
@@ -1,11 +1,10 @@
 from operator import itemgetter
 
 from space import *
-# import mdp
+from mdp import value_iteration
 
 def main():
 	mazey = maze()
-
 
 	listy = [(0,1,-1), (1,3,0), (1,4,-1), (2,3,0), (2,5,3), (3,3,0), (5,0,1), (5,1,-1), (5,3,0), (5,4,-1), (5,5,-1)]
 
@@ -17,6 +16,8 @@ def main():
 
 def policy(mazey, terminal = True):
 	#call value iteration here
+	value_iteration(mazey)
+	mdp.value
 	for i in range(mazey.size):
 		for j in range(mazey.size):
 			policy = max_of_neighbors(mazey, i, j)
