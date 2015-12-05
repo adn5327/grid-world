8df2f7dbc8898e49bfba95cf25192452755ec2b1
Jakub Klapacz <kubaklapacz@gmail.com>
2015-12-05 15:10:37 -0600

    Some changes to mdp

--- mdp.py
+++ mdp.py
@@ -1,3 +1,18 @@
 from space import *
 
-def
+actions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
+
+def action_utility(s, )
+
+def utility(spacey, maze_obj):
+	cur_y, cur_x = spacey[0], spacey[1]
+
+
+
+def value_iteration(iterations = 50, maze_obj, terminal = True):
+
+	for n in range(iterations):
+		for i in range(maze_obj.size):
+			for j in range(maze_obj.size):
+
+		# Change utilities here
