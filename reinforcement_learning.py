94c2ea2cdba8379d62b77fa61c7991aa5f49a4aa
Abhishek Deep Nigam <adn5327@gmail.com>
2015-12-06 18:40:20 -0600

    hopefully this is close to done now

--- reinforcement_learning.py
+++ reinforcement_learning.py
@@ -12,21 +12,19 @@ class Grid():
 		self.utility = dict()
 		self.actions = dict()
 		self.valid_actions()
-		self.iterations = list()
+		self.num_iterations = list()
 		self.gamma = 0.99
-		self.policy = {}
-		# number of times we've taken action from state s
+
 		self.n_s_a = dict()
-		# number of trials
-		self.trial = []
-		self.rmse = []
+		self.trial = list()
+		self.rmse = list()
 		self.maxtrial = 1000
-		self.maxstep = 50
-		# cutoff for exploration
+		self.convergence_number = 50
+
 		self.Ne = 20
-		# q values
-		self.q = {}
-		self.expectedUtil = {}
+
+		self.q = dict()
+		self.expectedUtil = dict()
 		self.terminal_list = list()
 
 	def alpha(self, t):
@@ -49,34 +47,24 @@ class Grid():
 			state_action = (state, action)
 			maxy.append(self.q.get(state_action,0))
 		return max(maxy)
-	def cal_rmse(self):
-		sum_diff = 0
+	def calculate_rmse(self):
+		summer = 0
 		for state in self.states:
-			true = self.utility[state][-1]
+			real_utility = self.utility[state][-1]
 			expect = self.expectedUtil[state][-1]
-			sum_diff += (true - expect)**2
-		return math.sqrt(sum_diff / len(self.states))
+			summer += (real_utility - expect)**2
+		return math.sqrt(summer / len(self.states))
 	def reinforcement_learning(self):
 		for trial in range(1,self.maxtrial):
 			self.trial.append(trial)
 			index = random.randint(0,len(self.states)-1)
 			state = self.states[index]
-			for t in range(1,self.maxstep+1):
+			for t in range(1,self.convergence_number+1):
 				alpha = self.alpha(t)
 				move = self.optimal_action(state)
-				col = state[0]
-				row = state[1]
-				if move == 'U':
-					new_state = (col, row-1)
-				elif move == 'L':
-					new_state = (col-1, row)
-				elif move == 'R':
-					new_state = (col+1, row)
-				else:
-					new_state = (col, row+1)
 
-				if new_state not in self.states:
-					new_state = (col, row)
+				new_state = self.get_new_state(state, move)
+
 				state_action = (state, move)
 				self.n_s_a[state_action] = self.n_s_a.get(state_action, 0) + 1
 				self.q[state_action] = self.q.get(state_action,0) + alpha * (self.rewards[state] \
@@ -86,9 +74,23 @@ class Grid():
 				if statey not in self.expectedUtil:
 					self.expectedUtil[statey] = list()
 				self.expectedUtil[statey].append(self.max_q(statey))
-			self.rmse.append(self.cal_rmse())
+			self.rmse.append(self.calculate_rmse())
 
+	def get_new_state(self, state, move):
+		col, row = state[0], state[1]
+		if move == 'U':
+			new_state = (col, row-1)
+		elif move == 'L':
+			new_state = (col-1, row)
+		elif move == 'R':
+			new_state = (col+1, row)
+		else:
+			new_state = (col, row+1)
 
+		if new_state not in self.states:
+			new_state = (col, row)
+
+		return new_state
 	def setup_grid(self):
 		for i in range(6):
 			for j in range(6):
@@ -120,79 +122,116 @@ class Grid():
 	def expect_utility(self, state, terminal):
 		if terminal:
 			if state in self.terminal_list:
-				self.policy[state] = '#'
 				return 0
-		col = state[0]
-		row = state[1]
+		col, row = state[0], state[1]
 		expect_util = dict()
 		for action in self.actions:
 			moves = self.actions[action]
 			sum_moves = 0
 			for move, prob in moves.iteritems():
-				if move == 'U':
-					new_state = (col, row-1)
-				elif move == 'L':
-					new_state = (col-1,row)
-				elif move == 'R':
-					new_state = (col+1, row)
-				else:
-					# move == 'D'
-					new_state = (col, row+1)
-				
-				if new_state not in self.states:
-					new_state = (col, row)
+				new_state = self.get_new_state(state, move)
 				
 				new_state_util = self.utility[new_state][-1]
 				sum_moves += prob * new_state_util
 			expect_util[action] = sum_moves
 		best_policy = max(expect_util.iteritems(), key = operator.itemgetter(1))[0]
-		self.policy[state] = best_policy
 		return expect_util[best_policy]
 
 
 	def value_iteration(self, terminal = True):
-		self.iterations.append(0)
-		for i in range(1,self.maxstep+1):
-			self.iterations.append(i)
+		self.num_iterations.append(0)
+		for i in range(1,self.convergence_number+1):
+			self.num_iterations.append(i)
 			for state in self.states:
 				new_util = self.rewards[state] + (self.gamma * self.expect_utility(state, terminal))
 				self.utility[state].append(new_util)
 
+	def max_of_neighbors(self, np_array, i , j):
+		find_max = list()
+		if i-1 > 0:
+			value = np_array[i-1][j]
+			find_max.append((value, 'U'))
+
+		if i+1 <len(np_array):
+			value = np_array[i+1][j]
+			find_max.append((value,'D'))
+		if j-1 >0:
+			value = np_array[i][j-1]
+			find_max.append((value, 'L'))
+		if j+1 <len(np_array):
+			value = np_array[i][j+1]
+			find_max.append((value,'R'))
+
+
+		return max(find_max,key=operator.itemgetter(0))[1]
+
+	def print_policy(self, np_array, terminal):
+		listy = [['0' for x in range(len(np_array))] for y in range(len(np_array))]
+		for i in range(len(np_array)):
+			for j in range(len(np_array[i])):
+				policy = self.max_of_neighbors(np_array, i, j)
+				if (j,i) not in self.utility.keys():
+					policy = 'w'
+				if (j,i) in self.terminal_list and terminal:
+					policy = 't'
+				listy[i][j] = policy
+		ret_str = ''
+		for each in listy:
+			for val in each:
+				ret_str+=val
+				ret_str+='\t'
+			ret_str+='\n'
+		print ret_str
+
+
+
+
 if __name__ == '__main__':
 	maze = Grid()
 	maze.setup_grid()
-	maze.value_iteration()
-	print "Utilities of all states:"
+	terminal = True
+	maze.value_iteration(terminal)
+	print "utilities"
+	arr = np.zeros((6,6), dtype=np.double)
+
 	ret_str = ''
 	for i in range(6):
 		for j in range(6):
 			state = (j,i)
+			
 			if state in maze.utility.keys():
 				ret_str += '{0:.4f}'.format(maze.utility[state][-1])
+				arr[i][j] = maze.utility[state][-1]
 			else:
 				ret_str += '0.0000'
+				arr[i][j] = 0
 			ret_str+='\t'
 		ret_str+='\n'
 	print ret_str
 
-	print "-----------------------"
-	print "Optimal policy:"
-	ret_str = ''
+	print
+	print
+	print
+
+	print "policy"
+	maze.print_policy(arr, terminal)
+
+	fp = FontProperties()
+	fp.set_size('xx-small')
+	plt.plot()
 	for i in range(6):
 		for j in range(6):
-			state = (j,i)
+			state = (i,j)
 			if state in maze.utility.keys():
-				ret_str += maze.policy[state]
-			else:
-				ret_str += 'w'
-			ret_str+='\t'
-		ret_str+='\n'
-	print ret_str
+				plt.plot(maze.num_iterations, maze.utility[state], label=str(state))
+	plt.xlabel('Num iterations')
+	plt.ylabel('Utility estimate')
+	plt.legend(prop=fp, loc='best')
+	plt.show()
 
 	maze.reinforcement_learning()
 
-	fontP = FontProperties()
-	fontP.set_size('xx-small')
+
 
 	plt.plot()
 	for i in range(6):
@@ -200,14 +239,14 @@ if __name__ == '__main__':
 			state = (i,j)
 			if state in maze.utility.keys():
 				plt.plot(maze.trial, maze.expectedUtil[state], label=str(state))
-	plt.xlabel('Number of trials')
-	plt.ylabel('Utility estimation')
-	plt.legend(prop=fontP, loc='best')
+	plt.xlabel('Num trials')
+	plt.ylabel('Utility estimate')
+	plt.legend(prop=fp, loc='best')
 	plt.show()
 
 	plt.plot(maze.trial, maze.rmse, label = 'RMSE')
-	plt.xlabel('Number of trials')
-	plt.ylabel('Root mean square error')
+	plt.xlabel('Num trials')
+	plt.ylabel('RMSE')
 	plt.legend(loc='best')
 	plt.show()
 
